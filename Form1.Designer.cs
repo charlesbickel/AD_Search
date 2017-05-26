0f6cd524dc6851e955ca210bfb0ed3603b167812
Charles Bickel <CBickel87@gmail.com>
2017-05-26 10:13:37 -0400

    Initial git push for this repo

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AD_Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.open_btn = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // open_btn
+            // 
+            this.open_btn.Location = new System.Drawing.Point(2, 12);
+            this.open_btn.Name = "open_btn";
+            this.open_btn.Size = new System.Drawing.Size(49, 27);
+            this.open_btn.TabIndex = 3;
+            this.open_btn.Text = "Open";
+            this.open_btn.UseVisualStyleBackColor = true;
+            this.open_btn.Click += new System.EventHandler(this.open_btn_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(57, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 238);
+            this.textBox2.TabIndex = 5;
+            // 
+            // save_btn
+            // 
+            this.save_btn.Location = new System.Drawing.Point(2, 45);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(49, 27);
+            this.save_btn.TabIndex = 6;
+            this.save_btn.Text = "Save";
+            this.save_btn.UseVisualStyleBackColor = true;
+            this.save_btn.Click += new System.EventHandler(this.save_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.open_btn);
+            this.Name = "Form1";
+            this.Text = "AD Search Users";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button open_btn;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button save_btn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices.AccountManagement;
+
+namespace AD_Search
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getusername(string fullname)
+        {
+            var firstlast = fullname.Trim().Split(new char[] { ' ' });
+
+            string firstname = firstlast[0];
+            string lastname = firstlast[1];
+
+            PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
+
+            UserPrincipal qbeUser = new UserPrincipal(ctx);
+            qbeUser.GivenName = firstname;
+            qbeUser.Surname = lastname;
+
+            PrincipalSearcher srch = new PrincipalSearcher(qbeUser);
+
+            foreach(var found in srch.FindAll())
+            {
+                textBox2.Text += found.SamAccountName + "\r\n";
+            }
+        }
+
+
+        private void open_btn_Click(object sender, EventArgs e)
+        {
+            Stream mystream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text Files|*.txt";
+            openFileDialog1.Title = "Select a Text File";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {      
+                if ((mystream = openFileDialog1.OpenFile()) != null)
+                {
+                    string[] lines = File.ReadAllLines(openFileDialog1.FileName);
+                    foreach (string line in lines)
+                        { 
+                            using (mystream)
+                            {
+                                getusername(line);
+                            }
+                        }
+                }
+            }
+        }
+
+        private void save_btn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Text File|*.txt";
+            saveFileDialog1.Title = "Save a text file";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                using(StreamWriter sw = new StreamWriter(saveFileDialog1.OpenFile()))
+                {
+                    sw.Write(textBox2.Text);
+                }
+            }
+        }
+    }
+}
